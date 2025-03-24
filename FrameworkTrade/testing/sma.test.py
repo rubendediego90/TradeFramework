4acd2bbd920f16f1c3a242e77643ec9050e32c90
rubendediego90 <rubendediego90@gmail.com>
2025-03-24 22:20:38 +0100

    cambio nombres

--- FrameworkTrade/testing/sma.test.py
+++ FrameworkTrade/testing/sma.test.py
@@ -6,36 +6,39 @@ import yfinance as yf
 
 def sma(array, period):
     series = pd.Series(array)
-    return series.rolling(window=period).mean().values  # Convertimos de nuevo a numpy array
+    return series.rolling(window=period).mean().values 
 
 class EstrategiaCrucesMA(Strategy):
+    sma_quick_data=22
+    sma_slow_data=112
+    
     def init(self):
-        self.sma50 = self.I(sma, self.data.Close, 50)   # SMA rápida
-        self.sma200 = self.I(sma, self.data.Close, 200)  # SMA lenta
+        self.smaQuick = self.I(sma, self.data.Close, self.sma_quick_data)
+        self.smaSlow = self.I(sma, self.data.Close, self.sma_slow_data)
 
     def next(self):
-        # Señal de compra (long): SMA50 cruza por encima de SMA200
-        if self.sma50[-1] > self.sma200[-1] and self.sma50[-2] <= self.sma200[-2]:  
+        # Señal de compra (long): smaQuick cruza por encima de smaSlow
+        if self.smaQuick[-1] > self.smaSlow[-1] and self.smaQuick[-2] <= self.smaSlow[-2]:  
             self.position.close()  # Cerramos cualquier posición anterior
             self.buy()  # Abrimos una posición larga
 
-        # Señal de venta (short): SMA50 cruza por debajo de SMA200
-        elif self.sma50[-1] < self.sma200[-1] and self.sma50[-2] >= self.sma200[-2]:
+        # Señal de venta (short): smaQuick cruza por debajo de smaSlow
+        elif self.smaQuick[-1] < self.smaSlow[-1] and self.smaQuick[-2] >= self.smaSlow[-2]:
             self.position.close()  # Cerramos cualquier posición anterior
             self.sell()  # Abrimos una posición corta
 
 # Cargar datos históricos desde Yahoo Finance
 symbol = "ETH-USD"
-data = yf.download(symbol, start="2025-01-25", end="2025-03-23", interval="5m")
-
-# Limpiar las columnas para que sean simples
+data = yf.download(symbol, start="2025-01-24", end="2025-03-23", interval="5m")
 data.columns = [col[0] for col in data.columns]
 
 # Asegurarnos de que 'Date' es el índice
 data.index = pd.to_datetime(data.index)
 
 # Crear y ejecutar el backtest
+
 bt = Backtest(data, EstrategiaCrucesMA)
+
 #Ejecutamos el backtest
 stats = bt.run()
 
